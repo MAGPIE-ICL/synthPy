d79da596e892193e11d5e72868a04676b2190c6a
le322 <le322@ic.ac.uk>
2024-06-28 19:01:20 +0100

    fixes to turbulence generator

--- field_generator/new_fft_1D.py
+++ field_generator/new_fft_1D.py
@@ -1,7 +1,6 @@
 import numpy as np
 
 def fft_1D(l_max, l_min, extent, res, spectrum):
-    print('we are in!')
     dx = extent / (res)
 
     x = np.arange(-extent, extent, dx)
@@ -11,14 +10,13 @@ def fft_1D(l_max, l_min, extent, res, spectrum):
     k = np.fft.fftfreq(2*res, d = dx) * 2 * np.pi
 
     S = np.where((k >= 2*np.pi/l_max) & (k< 2* np.pi / l_min), spectrum(k), 0)
-
-    std = (S/2)**0.5
+    std = np.sqrt((S/2))
 
     real = np.random.normal(0, std, k.shape)
 
     imag = np.random.normal(0, std, k.shape)
 
-    spectrum = (real + 1j*imag)*np.sqrt(S)
+    spectrum = (real + 1j*imag)
     noise = np.fft.ifft(spectrum)
 
     noise = np.abs(noise)
