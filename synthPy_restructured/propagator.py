3d9145c3bfc49fd7847d21552595faae96f8071f
Kepler7894i <69768113+Kepler7894i@users.noreply.github.com>
2025-07-02 08:13:39 +0100

    Update propagator.py
    added parameters to debug memory usage - testing for captured constants

--- synthPy_restructured/propagator.py
+++ synthPy_restructured/propagator.py
@@ -8,6 +8,8 @@ import os
 
 # defaults float data types to 64-bit instead of 32 for greater precision
 jax.config.update('jax_enable_x64', True)
+jax.config.update('jax_captured_constants_report_frames', -1)
+jax.config.update('jax_captured_constants_warn_bytes', 256*1024**2)
 
 #from scipy.interpolate import RegularGridInterpolator
 from scipy.integrate import odeint, solve_ivp
