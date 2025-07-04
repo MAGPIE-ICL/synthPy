c34b99a92e317d4e649929108fa264c39435d31a
Kepler7894i <69768113+Kepler7894i@users.noreply.github.com>
2025-07-04 10:16:06 +0100

    Specificed resources to cx3-legacy

--- examples/jobs/solver_comparisons.sh
+++ examples/jobs/solver_comparisons.sh
@@ -1,12 +1,6 @@
 #!/bin/sh
 #PBS -l walltime=3:59:00
-
-if [[ $(hostname) == "login-a" ]]; then
-  #PBS -l select=1:ncpus=4:mem=96gb:ngpus=1:gpu_type=RTX6000
-elif [[ $(hostname) == "login-ai.cx3.hpc.ic.ac.uk" ]]; then
-  #PBS -l select=1:ncpus=4:mem=96gb:ngpus=1:gpu_type=a100
-fi
-
+#PBS -l select=1:ncpus=4:mem=96gb:ngpus=1:gpu_type=RTX6000
 #PBS -j oe
 
 cd '/rds/general/user/sm5625/home/synthPy/'
