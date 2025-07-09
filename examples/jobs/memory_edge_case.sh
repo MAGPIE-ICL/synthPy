b5dc4f2c016f5e0ce60d4a6d8d51aae6b6692990
Kepler7894i <69768113+Kepler7894i@users.noreply.github.com>
2025-07-09 16:11:56 +0100

    Fixed module loading

--- examples/jobs/memory_edge_case.sh
+++ examples/jobs/memory_edge_case.sh
@@ -3,6 +3,7 @@
 #PBS -l select=1:ncpus=4:mem=96gb:ngpus=1:gpu_type=RTX6000
 #PBS -j oe
 
+module load tools/prod
 module load jax/0.3.25-foss-2022a-CUDA-11.7.0
 
 python -u examples/jobs/run_scripts/memory_edge_case.py -d 512
