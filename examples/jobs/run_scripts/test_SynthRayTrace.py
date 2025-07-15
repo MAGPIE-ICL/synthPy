72ff1ab7184c78a7ce7530f408a148f85f6c25db
Kepler7894i <69768113+Kepler7894i@users.noreply.github.com>
2025-07-15 16:34:17 +0100

    Reconfiguring

--- examples/jobs/run_scripts/test_SynthRayTrace.py
+++ examples/jobs/run_scripts/test_SynthRayTrace.py
@@ -16,7 +16,7 @@ config.jax_init()
 parser = argparse.ArgumentParser()
 parser.add_argument("-d", "--domain", type = int)
 parser.add_argument("-r", "--rays", type = int)
-parser.add_argument("-f". "--forceDevice", type = str)
+parser.add_argument("-f", "--force-device", type = str)
 parser.add_argument("-m", "--memory", type = int)
 args = parser.parse_args()
 
