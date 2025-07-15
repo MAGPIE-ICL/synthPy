f560a35818f17b414a9541a14d4418a9404f2cc0
Kepler7894i <69768113+Kepler7894i@users.noreply.github.com>
2025-07-15 09:41:56 +0100

    Added an interactive script to use in jupyter with gpu access, has lower rays so we can directly test domain allocation

---
+++ examples/jobs/interactive_SynthRayTrace_jupyter.sh
@@ -0,0 +1,26 @@
+#!/bin/sh
+#PBS -l walltime=3:59:00
+#PBS -l select=1:ncpus=4:mem=32gb:ngpus=1:gpu_type=RTX6000
+#PBS -j oe
+
+cd '/rds/general/user/sm5625/home/synthPy/'
+
+echo 'loading packages'
+
+eval "$(~/miniforge3/bin/conda shell.bash hook)"
+conda activate testing
+
+echo 'packages loaded successfully'
+
+echo 'starting job'
+
+#mkdir -p /rds/general/user/sm5625/home/synth_ray_trace
+python -u examples/jobs/run_scripts/test_SynthRayTrace.py -d 512 -r 8192
+python -u examples/jobs/run_scripts/test_SynthRayTrace.py -d 640 -r 8192
+python -u examples/jobs/run_scripts/test_SynthRayTrace.py -d 768 -r 8192
+python -u examples/jobs/run_scripts/test_SynthRayTrace.py -d 896 -r 8192
+python -u examples/jobs/run_scripts/test_SynthRayTrace.py -d 1024 -r 8192
+python -u examples/jobs/run_scripts/test_SynthRayTrace.py -d 1152 -r 8192
+python -u examples/jobs/run_scripts/test_SynthRayTrace.py -d 1280 -r 8192
+python -u examples/jobs/run_scripts/test_SynthRayTrace.py -d 1408 -r 8192
+python -u examples/jobs/run_scripts/test_SynthRayTrace.py -d 1536 -r 8192
