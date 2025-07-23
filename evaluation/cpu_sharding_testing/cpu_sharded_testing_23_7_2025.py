80f10a8bfbbbd634a814eba9c6d7fd9ccd8c16bc
Kepler7894i <69768113+Kepler7894i@users.noreply.github.com>
2025-07-23 12:08:34 +0100

    Back tracked cpu test to original case of testing propagator bugs, updated filepaths

--- evaluation/cpu_sharding_testing/cpu_sharded_testing_23_7_2025.py
+++ evaluation/cpu_sharding_testing/cpu_sharded_testing_23_7_2025.py
@@ -5,18 +5,9 @@ import os
 
 sys.path.insert(0, '/rds/general/user/sm5625/home/synthPy/src/simulator')
 
-import beam as beam_initialiser
-import domain as d
-import propagator as p
-import diagnostics as diag
-#import utils
-
 import importlib
-importlib.reload(beam_initialiser)
-importlib.reload(d)
+import propagator as p
 importlib.reload(p)
-importlib.reload(diag)
-#importlib.reload(utils)
 
 import argparse
 
@@ -25,11 +16,6 @@ parser.add_argument("-d", "--domain", type = int)
 parser.add_argument("-r", "--rays", type = int)
 parser.add_argument("-c", "--core", type = int)
 args = parser.parse_args()
-
-import config
-config.jax_init(force_device = force_device, core_limit = cores)
-
-import jax
 
 n_cells = 128
 if args.domain is not None:
@@ -40,6 +26,23 @@ if args.rays is not None:
     Np = args.rays
 
 print("\nStarting cpu sharding test run with", n_cells, "cells and", Np, "rays.")
+
+if args.core is not None:
+    core_limit = args.core
+else:
+    from multiprocessing import cpu_count
+    core_limit = cpu_count()
+
+assert "jax" not in sys.modules, "jax already imported: you must restart your runtime - DO NOT RUN THIS FUNCTION TWICE"
+os.environ['XLA_FLAGS'] = "--xla_force_host_platform_device_count=" + str(core_limit)
+os.environ["XLA_PYTHON_CLIENT_ALLOCATOR"] = "platform"
+os.environ["XLA_PYTHON_CLIENT_MEM_FRACTION"] = "0.9"
+os.environ["TF_GPU_ALLOCATOR"] = "cuda_malloc_async"
+
+import jax
+
+jax.config.update('jax_enable_x64', True)
+jax.config.update('jax_traceback_filtering', 'off')
 
 extent_x = 5e-3
 extent_y = 5e-3
@@ -54,7 +57,32 @@ import jax.numpy as jnp
 from scipy.constants import c
 from scipy.constants import e
 
-domain = d.ScalarDomain(lengths, n_cells, ne_type = "test_exponential_cos")
+class ScalarDomain():
+    def __init__(self, lengths, dim):
+        self.x_length, self.y_length, self.z_length = lengths[0], lengths[1], lengths[2]
+        self.x_n, self.y_n, self.z_n = dim, dim, dim
+
+        self.x = jnp.float32(jnp.linspace(-self.x_length / 2, self.x_length / 2, self.x_n))
+        self.y = jnp.float32(jnp.linspace(-self.y_length / 2, self.y_length / 2, self.y_n))
+        self.z = jnp.float32(jnp.linspace(-self.z_length / 2, self.z_length / 2, self.z_n))
+
+        self.XX, self.YY, _ = jnp.meshgrid(self.x, self.y, self.z, indexing = 'ij', copy = True)
+        self.ZZ = None
+
+        self.XX = self.XX.at[:, :].set(self.XX / 2e-3)
+        self.XX = self.XX.at[:, :].set(10 ** self.XX)
+
+        self.YY = self.YY.at[:, :].set(self.YY / 1e-3)
+        self.YY = self.YY.at[:, :].set(jnp.pi * self.YY)
+        self.YY = self.YY.at[:, :].set(2 * self.YY)
+        self.YY = self.YY.at[:, :].set(jnp.cos(self.YY))
+        self.YY = self.YY.at[:, :].set(1 + self.YY)
+
+        self.ne = self.XX * self.YY
+
+        self.ne = self.ne.at[:, :].set(1e24 * self.ne)
+
+domain = ScalarDomain(lengths, n_cells)
 
 lwl = 1064e-9
 
@@ -64,7 +92,31 @@ beam_size = extent_x
 ne_extent = probing_extent
 beam_type = 'circular'
 
-beam_definition = beam_initialiser.Beam(Np, beam_size, divergence, ne_extent)
+def init_beam(Np, beam_size, divergence, ne_extent):
+    s0 = jnp.zeros((9, Np))
+
+    t  = 2 * jnp.pi * np.random.randn(Np)
+
+    u  = np.random.randn(Np)
+
+    ϕ = jnp.pi * np.random.randn(Np)
+    χ = divergence * np.random.randn(Np)
+
+    s0 = s0.at[0, :].set(beam_size * u * jnp.cos(t))
+    s0 = s0.at[1, :].set(beam_size * u * jnp.sin(t))
+    s0 = s0.at[2, :].set(-ne_extent)
+
+    s0 = s0.at[3, :].set(c * jnp.sin(χ) * jnp.cos(ϕ))
+    s0 = s0.at[4, :].set(c * jnp.sin(χ) * jnp.sin(ϕ))
+    s0 = s0.at[5, :].set(c * jnp.cos(χ))
+
+    s0 = s0.at[6, :].set(1.0)
+    s0 = s0.at[8, :].set(0.0)
+    s0 = s0.at[7, :].set(0.0)
+
+    return s0
+
+beam_definition = init_beam(Np, beam_size, divergence, ne_extent)
 
 rf, Jf, duration = p.solve(
     s0,
