1311ded28c827eadf106d51655e193aac3f2bdab
Sam MacKay <69768113+Kepler7894i@users.noreply.github.com>
2025-07-24 22:18:52 +0100

    Update cpu_sharded_testing_23_7_2025.py

--- evaluation/cpu_sharding_testing/cpu_sharded_testing_23_7_2025.py
+++ evaluation/cpu_sharding_testing/cpu_sharded_testing_23_7_2025.py
@@ -205,15 +205,15 @@ with jax.checking_leaks():
         grad = jnp.zeros_like(r)
 
         dndx = -0.5 * c ** 2 * jnp.gradient(ne / (3.14207787e-4 * omega ** 2), coordinates[:, 0], axis = 0)
-        grad = grad.at[0, :].set(trilinearInterpolator(coordinates, length, dim, dndx, r.T, fill_value = 0.0))
+        #grad = grad.at[0, :].set(trilinearInterpolator(coordinates, length, dim, dndx, r.T, fill_value = 0.0))
         del dndx
 
         dndy = -0.5 * c ** 2 * jnp.gradient(ne / (3.14207787e-4 * omega ** 2), coordinates[:, 1], axis = 1)
-        grad = grad.at[1, :].set(trilinearInterpolator(coordinates, length, dim, dndy, r.T, fill_value = 0.0))
+        #grad = grad.at[1, :].set(trilinearInterpolator(coordinates, length, dim, dndy, r.T, fill_value = 0.0))
         del dndy
 
         dndz = -0.5 * c ** 2 * jnp.gradient(ne / (3.14207787e-4 * omega ** 2), coordinates[:, 2], axis = 2)
-        grad = grad.at[2, :].set(trilinearInterpolator(coordinates, length, dim, dndz, r.T, fill_value = 0.0))
+        #grad = grad.at[2, :].set(trilinearInterpolator(coordinates, length, dim, dndz, r.T, fill_value = 0.0))
         del dndz
 
         return grad
