63a893fdc31eab769217ed484ea1279fd3312c21
Sam MacKay <69768113+Kepler7894i@users.noreply.github.com>
2025-07-24 12:35:49 +0100

    Create test_cpu_CWE_single.sh

---
+++ evaluation/cpu_sharding_testing/test_cpu_CWE_single.sh
@@ -0,0 +1,17 @@
+#!/bin/sh
+#PBS -l walltime=3:59:00
+#PBS -l select=1:ncpus=8:mem=24gb
+#PBS -j oe
+
+cd '/rds/general/user/sm5625/home/synthPy/'
+
+echo 'loading packages'
+
+eval "$(~/miniforge3/bin/conda shell.bash hook)"
+conda activate testing
+
+echo 'packages loaded successfully'
+
+echo 'starting job'
+
+python -u evaluation/cpu_sharding_testing/cpu_sharded_testing_23_7_2025.py -d 1536 -r 8192 -c 8
