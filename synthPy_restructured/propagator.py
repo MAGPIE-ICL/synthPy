Using an explicit line integral instead of a derivative to calculate phase gives correct results. Working on getting this to worl with Jax. Also added a few more test domains and bug fixes

synthPy_restructured/propagator.py
@@ -199,7 +199,8 @@ class Propagator:
     def phase(self,x):
         if(self.phaseshift):
             #self.refractive_index_interp = RegularGridInterpolator((self.ScalarDomain.x, self.ScalarDomain.y, self.ScalarDomain.z), self.n_refrac(), bounds_error = False, fill_value = 1.0)
-            return self.omega*(self.refractive_index_interp(x.T)-1.0)
+            # return self.omega*(self.refractive_index_interp(x.T)-1.0)
+            return 0.0
         else:
             return 0.0
     
@@ -233,7 +234,8 @@ class Propagator:
         # Need to make sure all rays have left volume
         # Conservative estimate of diagonal across volume
         # Then can backproject to surface of volume
-
+        self.prev_x = None
+        self.phase_integral = 0
         s0 = self.Beam.s0
 
         # 8.0^0.5 is an arbritrary factor to ensure rays have enough time to escape the box
@@ -258,7 +260,7 @@ class Propagator:
             def dsdt_ODE(t, y, args):
                 return dsdt(t, y, args[0], args[1]) * norm_factor
 
-            def diffrax_solve(dydt, t0, t1, Nt, rtol=1e-1, atol=1e-5):
+            def diffrax_solve(dydt, t0, t1, Nt, rtol=1e-4, atol=1e-5):
                 """
                 Here we wrap the diffrax diffeqsolve function such that we can easily parallelise it
                 """
@@ -326,8 +328,7 @@ class Propagator:
             #print(sol.ys)
             
         print("phase shift from line integral:", self.phase_integral)
-        self.prev_x = None
-        self.phase_integral = 0
+        
 
         finish = time()
         self.duration = finish - start
@@ -458,12 +459,12 @@ def dsdt(t, s, Propagator, parallelise):
     x = s[:3, :]
     v = s[3:6, :]
 
-    # if Propagator.phaseshift is True:
-    #     if Propagator.prev_x is not None:
-    #         dr = distance(x, Propagator.prev_x)
-    #         Propagator.phase_integral -= Propagator.ne_interp(x.T)*dr/Propagator.nc*np.pi/Propagator.Beam.wavelength
+    if Propagator.phaseshift is True:
+        if Propagator.prev_x is not None:
+            dr = distance(x, Propagator.prev_x)
+            Propagator.phase_integral -= Propagator.ne_interp(x.T)*dr/Propagator.nc*np.pi/Propagator.Beam.wavelength
 
-    #     Propagator.prev_x = x
+        Propagator.prev_x = x
 
     # Amplitude, phase and polarisation
     a = s[6, :]

synthPy_restructured/x_ray_diffraction.py
@@ -1,6 +1,7 @@
 import numpy as np
 from scipy.signal.windows import tukey
 from scipy.interpolate import CloughTocher2DInterpolator as CT2D
+from scipy.interpolate import LinearNDInterpolator as LND
 
 #@staticmethod
 def prepare_field_for_propagation(U0, pad_factor = 2, alpha = 0.4):
@@ -81,10 +82,10 @@ def propagate(Propagator, jones_vector, amplitudes, phases, z, pad_factor = 2):
     x_positions = jones_vector[0]
     y_positions = jones_vector[2]
     amplitudes = amplitudes[:, -1]
-    phases = phases[:, -1]
+    #phases = phases[:, -1]
 
-    phases_interp = CT2D((x_positions, y_positions), phases, fill_value = 0.0)
-    amplitudes_interp = CT2D((x_positions, y_positions), amplitudes, fill_value = 0.0)
+    phases_interp = LND((x_positions, y_positions), phases, fill_value = 0.0)
+    amplitudes_interp = LND((x_positions, y_positions), amplitudes, fill_value = 0.0)
 
     XX, YY = np.meshgrid(Propagator.ScalarDomain.x, Propagator.ScalarDomain.y)
     phase_grid = phases_interp((XX, YY))