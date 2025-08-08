e379b979a1b6e34b2271791f57ce554770a2df2b
Kepler7894i <69768113+Kepler7894i@users.noreply.github.com>
2025-08-08 15:22:22 +0100

    Temporary solution

--- src/simulator/domain.py
+++ src/simulator/domain.py
@@ -199,10 +199,10 @@ class ScalarDomain(eqx.Module):
         del dims
         del valid_types
 
-        print(self.dims)
-        print(domain_estimate(self.dims))
-        print(np.int64(domain_estimate(self.dims)))
-        predicted_domain_allocation = np.int64(domain_estimate(self.dims))
+        #print(self.dims)
+        #print(domain_estimate(self.dims))
+        #print(np.int64(domain_estimate(self.dims)))
+        predicted_domain_allocation = dim[0] * dim[1] * dim[2] * conv#np.int64(domain_estimate(self.dims))
         print("Predicted size in memory of domain:", mem_conversion(predicted_domain_allocation))
 
         if iteration == 1 and auto_batching:
